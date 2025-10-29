b867f3440de94c2d0fd312a219b1e3ddf6ce8740
Emmanuelmadva <madvaemmanuel@gmail.com>
2025-10-29 01:20:12 +0100

    up auth

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from database.connection import Base, engine
 from fastapi import FastAPI
 from app.services.animals.models import Animal
 from app.services.animals.routers import animal_router
-
+from app.services.auth.routers import auth_router
 print("Création des tables...")
 Base.metadata.create_all(bind=engine)
 print("Tables créées avec succès !")
@@ -11,3 +11,4 @@ app = FastAPI(title="RanchSmart API")
 
 # Ajouter le router animals
 app.include_router(animal_router, prefix="/animals", tags=["Animals"])
+app.include_router(auth_router, prefix="/auth", tags=["Authentification"])
